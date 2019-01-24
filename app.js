e89fc9382f2a1c9a1e95df89bdb7c9a342db75ea
Jim Disenstam <jim.disenstam@gmail.com>
2019-01-24 09:29:38 +0100

    Getting links

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 'use strict'
 
 const scraper = require('./lib/scraper')
+const query = require('./lib/query')
 
 scraper.scrape('http://vhost3.lnu.se:20080/weekend')
+  .then(res => query.getLinks(res))
   .then(res => console.log(res))
   .catch(e => console.error(e))
